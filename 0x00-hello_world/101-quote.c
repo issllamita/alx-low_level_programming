9eaf1127329e1e76b97083fabadd1b8be3454740
Mrifag Islam <mrifag.i.slam@gmail.com>
2023-04-20 18:54:19 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  * main - program that print a string to the standar error
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
