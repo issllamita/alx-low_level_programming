f1454aa2ec8359f21ec0685eaa254f2472ac2cab
Mrifag Islam <mrifag.i.slam@gmail.com>
2023-05-12 12:29:48 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - prints name of the program itself
+  * @argc: integer
+  * @argv: array of string
+  * Return: always 0
+  */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
