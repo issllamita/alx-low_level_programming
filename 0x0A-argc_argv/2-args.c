9e6867edc34e4c7ad3307b55d76f0b474b8b4cda
Mrifag Islam <mrifag.i.slam@gmail.com>
2023-05-12 13:15:41 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - prints all arguments it receives
+  * @argc: number of command line arguments
+  * @argv: array of string
+  * Return: always 0
+  */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
