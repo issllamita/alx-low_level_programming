d96e2877d1cf9f64108c516f273edfd767f56aaf
Mrifag Islam <mrifag.i.slam@gmail.com>
2023-05-22 12:42:50 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,6 @@
-#ifndef DOG_H
-#define DOG_H
-/**
-  * struct dog - our own type that stor different type of data
-  * @name: name of dog
-  * @age: age of dog
-  * @owner: owner name
-  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
