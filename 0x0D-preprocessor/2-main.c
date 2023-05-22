86236bfc46af9979ef716ee8244619ec07e4d409
Mrifag Islam <mrifag.i.slam@gmail.com>
2023-05-22 11:40:30 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - prg  that prints the name of the file it was compiled from
+  * Return: always 0
+  */
+int main(void)
+{
+	printf(__FILE__ "\n");
+	return (0);
+}
