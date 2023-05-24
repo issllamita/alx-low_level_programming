95faba469ab8927d696b0f9768000a1593813a50
Mrifag Islam <mrifag.i.slam@gmail.com>
2023-05-24 12:09:36 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+  * print_name - function that print a name
+  * @name: pointer to string
+  * @f: pointer to function
+  * Return: nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL || name == NULL)
+	{
+		return;
+	}
+	f(name);
+}
+
+
