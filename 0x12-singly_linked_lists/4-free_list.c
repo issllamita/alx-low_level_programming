769c06f78b9638f056db9d61858ac3307aadc9f1
Mrifag Islam <mrifag.i.slam@gmail.com>
2023-06-01 14:46:50 -0700

    update

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+  * free_list - frees a list_t
+  * @head: double pointer to the head of list_t
+  */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
